43e1dc7278e24a7807d91545564155dd657ffcdc
Harald Bildsøe <harb@bunker-holding.com>
2020-07-16 19:13:20 +0200

    add more tests

--- src/DrinksUI.Test/Data/DbContext.cs
+++ src/DrinksUI.Test/Data/DbContext.cs
@@ -1,48 +1,84 @@
-using NUnit.Framework;
-using DrinksUI.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
 using DrinksUI.Data;
-using Microsoft.EntityFrameworkCore;
+using DrinksUI.Data.Models;
+using DrinksUI.Dtos;
 using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 
-namespace DrinksUI.Test
+namespace DrinksUI.Test.Data
 {
-    public class Tests
+    public class DbContext
     {
+        private DrinkContext _context;
+        private SqliteConnection _connection;
+
         [SetUp]
         public void Setup()
         {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
 
+            var options = new DbContextOptionsBuilder<DrinkContext>()
+                .UseSqlite(_connection)
+                .Options;
 
+            _context = new DrinkContext(options);
+            _context.Database.EnsureCreated();
+        }
 
+        [TearDown]
+        public void TearDown()
+        {
+            _connection.Close();
         }
 
         [Test]
-        public void Test1()
+        public void CanInsertAndRetrieveIngredients()
         {
-            // In-memory database only exists while the connection is open
-            var connection = new SqliteConnection("DataSource=:memory:");
-            connection.Open();
-            
-            try
-            {
-                var options = new DbContextOptionsBuilder<DrinkContext>()
-                    .UseSqlite(connection)
-                    .Options;
-                
-                using (var context = new DrinkContext(options))
-                {
-                    context.Database.EnsureCreated();
-                }
+            var ingredients = new List<IngredientModel>(){
+                new IngredientModel(){Type = "Vodka", AddiType = AddiType.PushDosed, Unit = Unit.CL},
+                new IngredientModel(){Type = "Lemon Slice", AddiType = AddiType.Extra, Unit = Unit.Pcs}
+            };
 
-                using (var context = new DrinkContext(options))
-                {
+            _context.AddRange(ingredients);
+            _context.SaveChanges();
 
-                }
-            }
-            finally
+            var vodka = _context.Ingredients.FirstOrDefault(x => x.Type == "Vodka");
+            Assert.That(vodka, Is.Not.Null);
+            Assert.That(vodka.AddiType, Is.Not.Null);
+            Assert.That(vodka.Unit, Is.Not.Null);
+        }
+
+        [Test]
+        public void CanInsertAndRetrieveDrink()
+        {
+            var ingredients = new List<IngredientModel>(){
+                new IngredientModel(){Type = "Vodka", AddiType = AddiType.PushDosed, Unit = Unit.CL},
+                new IngredientModel(){Type = "Lemon Slice", AddiType = AddiType.Extra, Unit = Unit.Pcs}
+            };
+
+            _context.AddRange(ingredients);
+            _context.SaveChanges();
+
+            _context.Add(new DrinkModel()
             {
-                connection.Close();
-            }
+                Name = "screwDriver",
+                description = "Something That ReSpeller Won't hate",
+                Addis = new List<AddiModel>()
+                {
+                    new AddiModel() {Ingredient = ingredients[0], Amount = 2},
+                    new AddiModel() {Ingredient = ingredients[1], Amount = 14}
+                }
+            });
+
+            _context.SaveChanges();
+
+            var drink = _context.Drinks.FirstOrDefault();
+            Assert.That(drink, Is.Not.Null);
+            Assert.That(drink.description, Is.Not.Null);
+            Assert.That(drink.Addis, Has.Count.EqualTo(2));
         }
     }
 }
