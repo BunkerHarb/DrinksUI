5e80bf7affb7781708e997ac61f3d2c4d180b139
Harald Bildsøe <harb@bunker-holding.com>
2020-07-16 20:48:26 +0200

    Create DTOs

---
+++ src/DrinksUI.Dtos/implementations/IngredientDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.NetworkInformation;
+using DrinksUI.Dtos.Interfaces;
+
+namespace DrinksUI.Dtos.implementations
+{
+    public class IngredientDto : IIngredient
+    {
+        public int Id { get; }
+        public string Type { get; set; }
+        public AddieType AddieType { get; set; }
+        public Unit Unit { get; set; }
+
+        public IngredientDto(int id, string type, AddieType addieType, Unit unit)
+        {
+            Id = id;
+            Type = type;
+            AddieType = addieType;
+            Unit = unit;
+        }
+    }
+}
